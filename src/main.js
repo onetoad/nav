40f743122cd03591ed7053456335ea05c0247adc
onetoad <onetoad@qq.com>
2019-12-16 00:19:40 +0800

    v1

---
+++ src/main.js
@@ -0,0 +1,82 @@
+const $siteList = $(".siteList");
+const $lastLi = $siteList.find("li.last");
+const x = localStorage.getItem("x");
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+  { logo: "A", url: "https://www.acfun.cn" },
+  { logo: "B", url: "https://www.bilibili.com" },
+  { logo: "C", url: "http://cc.163.com/" },
+  { logo: "D", url: "https://www.douyu.com/" },
+  { logo: "F", url: "https://www.figma.com/files/recent" },
+  { logo: "G", url: "https://github.com/" },
+  { logo: "H", url: "https://www.huaweicloud.com/?locale=zh-cn" }
+];
+const simplifyUrl = url => {
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .replace(/\/.*/, ""); // 删除 / 开头的内容
+};
+
+const render = () => {
+  $siteList.find("li:not(.last)").remove();
+  hashMap.forEach((node, index) => {
+    const $li = $(`<li>
+      <div class="site">
+        <div class="logo">${node.logo}</div>
+        <div class="link">${simplifyUrl(node.url)}</div>
+        <div class="close">
+          <svg class="icon">
+            <use xlink:href="#icon-delete"></use>
+          </svg>
+        </div>
+      </div>
+    </li>`).insertBefore($lastLi);
+    $li.on("click", () => {
+      window.open(node.url);
+    });
+    $li.on("click", ".close", e => {
+      e.stopPropagation(); // 阻止冒泡
+      hashMap.splice(index, 1);
+      render();
+    });
+  });
+};
+
+render();
+
+$(".addButton").on("click", () => {
+  let url = window.prompt("请问你要添加的网址是啥？");
+  if (url.indexOf("http") !== 0) {
+    url = "https://" + url;
+  }
+  console.log(url);
+  hashMap.push({
+    logo: simplifyUrl(url)[0].toUpperCase(),
+    url: url
+  });
+  render();
+});
+
+window.onbeforeunload = () => {
+  const string = JSON.stringify(hashMap);
+  localStorage.setItem("x", string);
+};
+
+$(document).on("keypress", e => {
+  const { key } = e;
+  for (let i = 0; i < hashMap.length; i++) {
+    if (hashMap[i].logo.toLowerCase() === key) {
+      window.open(hashMap[i].url);
+    }
+  }
+});
+
+let backgroundIndex = 1;
+$(".refresh-background", ".refresh").click(() => {
+  $("body").removeClass(`img${backgroundIndex}`);
+  if (backgroundIndex === 9) backgroundIndex = 0;
+  backgroundIndex++;
+  $("body").addClass(`img${backgroundIndex}`);
+});
